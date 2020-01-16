94a07c3eafbf47eb9d320d20928260a89e54a668
Andrei Mirea <andrei.mirea@tremend.com>
2020-01-16 14:53:29 +0200

    table 1 sorted

--- src/test/java/com/selenium/template/page_objects/SeleniumEasy/Table/TableDataSearchPage.java
+++ src/test/java/com/selenium/template/page_objects/SeleniumEasy/Table/TableDataSearchPage.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TableDataSearchPage extends PageBase {
 
@@ -31,6 +33,9 @@ public class TableDataSearchPage extends PageBase {
     @FindBy(css="#task-table > tbody > tr:nth-of-type(1) > td:nth-of-type(4)")
     private WebElement status;
 
+    @FindBy(xpath="//td[contains(text(),'No results found')]")
+            private WebElement noResultsElem;
+
     WebElement[] table1Elems = {hashtag, task, assignee, status};
     //------------------------------------------------------- Table 2 Elements
 
@@ -44,7 +49,7 @@ private WebElement tableTwo;
     @FindBy(xpath = "//button[@class='btn btn-default btn-xs btn-filter']")
     private WebElement filterButton;
 
-    //---------------------------------------------------------- Table 1 Methods
+    //---------------------------------------------------------- Table 1 Methods (POSITIVE)
 
   public boolean searchTableOne(String[] keywords){
       boolean check1 = true;
@@ -57,8 +62,21 @@ private WebElement tableTwo;
           }
       }
 }
+//----------------------------------------------------------------- Table 1 Methods (NEGATIVE)
 
+    public boolean noResults(String garbage){
+      filterOne.sendKeys(garbage);
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(noResultsElem);
+        return driver.findElement(noResultsElem).isEnabled();
+    }
+//-----------------------------------------------------------Table 2 Methods
 
+    public void clickFilter(){
+      filterButton.click();
+    }
 
+    public boolean filterTable(String[] keywords){
 
+    }
 }
