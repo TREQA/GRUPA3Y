8d5a07449fbad22cca564f09e00fe5f74a68bac1
Andrei Mirea <andrei.mirea@tremend.com>
2020-01-17 11:32:36 +0200

    date pickers 1 in progress

--- src/test/java/com/selenium/template/page_objects/SeleniumEasy/DatePickers/DatePicker_Bootstrap_Page.java
+++ src/test/java/com/selenium/template/page_objects/SeleniumEasy/DatePickers/DatePicker_Bootstrap_Page.java
@@ -1,6 +1,7 @@
 package com.selenium.template.page_objects.SeleniumEasy.DatePickers;
 import com.selenium.template.page_objects.PageBase;
 import com.selenium.template.utils.SeleniumWrapper;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,6 +11,35 @@ public class DatePicker_Bootstrap_Page extends PageBase {
     public DatePicker_Bootstrap_Page(WebDriver driver) {
         super(driver);
     }
+
+    //------------------------------------------------------------------------------ Date Picker One Elements
+
+    @FindBy(xpath="//div[@class='input-group date']//span[@class='input-group-addon']")
+    private WebElement datePickerbuttonOne;
+
+    @FindBy(css="div.datepicker-days > table.table-condensed > thead > tr:nth-of-type(2) > th.datepicker-switch")
+    private WebElement monthButtonOne;
+
+    @FindBy(xpath="//div[@class='datepicker-months']//th[@class='prev'][contains(text(),'«')]")
+    private WebElement prevButtonOne;
+
+    private WebElement checkYearOne(String year) {
+        return driver.findElement(By.xpath("//div[@class='datepicker-months']//th[@class='datepicker-switch'][contains(text(),'"+year+"')]"));
+    }
+
+    private WebElement chooseMonthOne(String month) {
+        return driver.findElement(By.xpath("//span[contains(text(),'"+month+"')]"));
+    }
+
+    private WebElement chooseDayOne(String day) {
+        return driver.findElement(By.xpath("//td[contains(text(),'"+day+"')]"));
+    }
+
+    @FindBy   //input[@placeholder='dd/mm/yyyy']
+
+//------------------------------------------------------------------------------------------Date Picker 2 Elements
+
+
 
 
 
